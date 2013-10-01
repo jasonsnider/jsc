f1736b487c5899f0d03828f2d9611064747a8404
Jason Snider <jsnider77@gmail.com>
2013-10-01 14:20:34 -0500

    Turned on model loading

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -71,6 +71,11 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
+CakePlugin::loadAll(
+    array(
+        'Users'=>array('routes'=>true)
+    )
+);
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter. By default CakePHP bundles two filters:
