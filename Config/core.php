2ba65190bf1cdf01e2d69c847b4a7fc5e53d6d25
Jason Snider <jsnider77@gmail.com>
2013-10-01 14:16:12 -0500

    Added salt and cipher configuration values

--- Config/core.php
+++ Config/core.php
@@ -224,12 +224,18 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write(
+		'Security.salt', 
+		'UpyUr7YTUsnh7RfURVhXtj1BMXlGqPbleLgGSezNHX4PPjKGf2EKlFm7CIO4x0qNMHtDW4qD1vtRPey5gdQBUdKxWxCty3gkLLZH'
+	);
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write(
+		'Security.cipherSeed', 
+		'8438987702835794979825386614311154053724017587974967595167599715220503904763561058808424184155678738'
+	);
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
